8b5e6c08dc670f362ce9485338ff885fa0ca3230
mael-10 <maelprogrammer1@gmail.com>
2024-04-25 18:02:45 -0300

    Modificado o while_loop arquivo

--- C_programms/aula_13_while_loop.c
+++ C_programms/aula_13_while_loop.c
@@ -1,6 +1,5 @@
 #include <stdio.h> 
 #include <locale.h>
-#include <windows.h>
 
 int main() { 
     setlocale(LC_ALL, "Portuguese_Brazil");
@@ -12,6 +11,19 @@ int main() {
         printf("%d\n", i);
         i++;
     }
+
+    printf("\n \n");
+
+    i = 0;
+
+    do {
+        printf("%d\n", i);
+        i++;
+    }
+    while (i < 5);
+
+
+    //A diferença entre o do/while e o while, é que o while verifica a condição primeiro, e o do/while executa primeiro e dps verefica a condição
 
 
     return 0; 
